4fe9c3089aa262515e9814e991e91db860fd6e4e
farshadmomo <momtazfarshad@gmail.com>
2025-01-16 12:02:23 +0330

    video functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React from 'react';
+import YouTube from 'react-youtube';
 import Navbar from "./Navbar";
-import './home.css';
 import PorscheHome from "./PorscheHome";
+import './home.css'
 
-export default function Home() {
-    return(
-        <div>
-            <Navbar />
-           
-            <PorscheHome />
-        </div>
+class Home extends React.Component {
+    render() {
+        const opts = {
+            height: '850',
+            width: '1519.2',
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 1,
+            },
+        };
+
+        return (
+            <div>
+                <Navbar />
+                <PorscheHome />
+                <YouTube videoId="478Ay_lNpT4?si=k4vOfmsH2bM9nnTx" opts={opts} onReady={this._onReady} />
+            </div>
     )
+    }
+
+    _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
 }
+
+export default Home
