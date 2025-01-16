3abbbe336ec8a00f2a8f9dd0957ca40081e981c1
farshadmomo <momtazfarshad@gmail.com>
2025-01-16 12:12:23 +0330

    videooos

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,25 @@ import React from 'react';
 import YouTube from 'react-youtube';
 import Navbar from "./Navbar";
 import PorscheHome from "./PorscheHome";
+import './home.css'
 
-export default function Home() {
-    return(
-        <div>
-            <Navbar />
-           
-            <PorscheHome />
-        </div>
+class Home extends React.Component {
+    render() {
+        const opts = {
+            height: '850',
+            width: '1519.2',
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 1,
+            },
+        };
+
+        return (
+            <div>
+                <Navbar />
+                <PorscheHome />
+                <YouTube videoId="478Ay_lNpT4?si=k4vOfmsH2bM9nnTx" opts={opts} onReady={this._onReady} />
+            </div>
     )
     }
 
